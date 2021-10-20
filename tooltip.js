7ce4146c503cbd26e443887d05e6057dedbb4597
Maro de Melo <marodemelo10@gmail.com>
2021-10-20 14:37:46 -0300

    usando tags de estilo no shadow dom

--- tooltip.js
+++ tooltip.js
@@ -3,36 +3,41 @@ class Tooltip extends HTMLElement {
     super();
     this._tooltipContainer;
     this._tooltipText = "Some dummy tooltip text.";
-    this.attachShadow({mode: 'open'})
-    const template = document.querySelector('#tooltip-template')
-    this.shadowRoot.appendChild(template.content.cloneNode(true))
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+    <style>
+      div {
+        background-color: black;
+        color: white;
+        position: absolute;
+        z-index: 10
+      }
+    </style>
+    <slot>Some default value</slot>
+    <span>(?)</span>
+    `;
   }
   connectedCallback() {
-      if(this.hasAttribute('text')){
-        this._tooltipText = this.getAttribute('text')
-      }
-    
-    const tooltipIcon = this.shadowRoot.querySelector("span");    
+    if (this.hasAttribute("text")) {
+      this._tooltipText = this.getAttribute("text");
+    }
+
+    const tooltipIcon = this.shadowRoot.querySelector("span");
     tooltipIcon.addEventListener("mouseenter", this._showTooltip.bind(this));
     tooltipIcon.addEventListener("mouseleave", this._hideTooltip.bind(this));
     this.shadowRoot.appendChild(tooltipIcon);
-    this.style.position="relative"
+    this.style.position = "relative";
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement("div");
-    this._tooltipContainer.textContent = this._tooltipText
-    this._tooltipContainer.style.width="200px"
-    this._tooltipContainer.style.backgroundColor="black"
-    this._tooltipContainer.style.color="white"
-    this._tooltipContainer.style.position="absolute"
-    this._tooltipContainer.style.zIndex="10"
-    this.shadowRoot.appendChild(this._tooltipContainer)
+    this._tooltipContainer.textContent = this._tooltipText;
+    this._tooltipContainer.style.width = "200px";   
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.shadowRoot.removeChild(this._tooltipContainer) 
-    
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
