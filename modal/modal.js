21acda8485a4ef5cae2aa075eda4a8245a5080ac
Maro de Melo <marodemelo10@gmail.com>
2021-10-24 15:01:58 -0300

    estilizando o modal

--- modal/modal.js
+++ modal/modal.js
@@ -19,14 +19,48 @@ class Modal extends HTMLElement {
             left:25%;
             width: 50%;
             z-index: 100;
-            background: white;
-            height: 30rem;
+            background: white;            
             border-radius: 3px;
-            box-shadow: 0 2px 8px rgba(0,0,0,0.26)
+            box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+        }
+        header{
+            padding: 1rem;
+        }
+
+        header h1{
+            font-size: 1.25rem;
+        }
+
+        #actions{
+            border-top:1px solid #ccc;
+            padding: 1rem;
+            display: flex;
+            justify-content: flex-end;
+        }
+        #actions button{
+            margin: 0 0.25rem;
+        }
+
+        #main {
+            padding: 1rem;
         }
     </style>
     <div id="backdrop"></div>
-    <div id="modal"></div>
+    <div id="modal">    
+        <header>
+            <h1>Please Confirm</h1>
+        </header>
+        <section id="main">
+            <slot></slot>
+        </section>
+        <section id="actions">
+            <button>Cancel</button>
+            <button>Confirm</button>
+        </section>
+    </div>
     `;
   }
 }
